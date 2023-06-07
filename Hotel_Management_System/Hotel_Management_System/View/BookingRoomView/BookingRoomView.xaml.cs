06d212f7cd641307b81b83344b13450e3568e632
Huynh Tien Phat <2409huynhphat@gmail.com>
2023-06-07 15:43:56 +0700

    Add feature cancel PhieuDatPhong and Add UI EditBookingRoom window

---
+++ Hotel_Management_System/Hotel_Management_System/View/BookingRoomView/BookingRoomView.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Hotel_Management_System.View.BookingRoomView
+{
+    /// <summary>
+    /// Interaction logic for BookingRoomView.xaml
+    /// </summary>
+    public partial class BookingRoomView : Window
+    {
+        public BookingRoomView()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_PreviewTextInput2(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsNumeric2(e.Text);
+        }
+
+        private bool IsNumeric2(string text)
+        {
+            // Kiểm tra xem chuỗi có chứa toàn số hay không
+            return int.TryParse(text, out int result);
+        }
+
+        private void TextBox_PreviewKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Back && ((TextBox)sender).Text.Length == 1)
+            {
+                ((TextBox)sender).Text = "0";
+            }
+            else if (((TextBox)sender).Text == "0" && e.Key >= Key.D0 && e.Key <= Key.D9)
+            {
+                ((TextBox)sender).Text = "";
+            }
+        }
+    }
+}
